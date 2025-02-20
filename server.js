70fd69487c53568fa86027db2b9e8e69f0acc748
Dave jal <newagesocietyaltsoc@gmail.com>
2025-02-20 23:37:20 +0000

    c3

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Connect to MongoDB
